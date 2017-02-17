c0e6648364f25bf4c1543335ceddfb4d963933d7
svg153 <svg153@gmail.com>
2017-02-17 12:52:46 +0100

    Added:  * apt updated  * check if python is installed Some minor fixes Added some TODOs

--- install.sh
+++ install.sh
@@ -15,18 +15,45 @@ echo ""
 echo -n "[++] Checking for dependencies..."
 echo -e "${ENDC}"
 
+echo -e -n "${HEADER}\t[+] Updating apt... please type your pass..."
+sudo apt-get update -qq
+
+if [ $? -eq 0 ]; then
+    echo -e "${OK}OK${ENDC}"
+else
+    echo -e "${ERROR}ERROR${ENDC}"
+fi
+
+# TODO: check if python is python 2.7
+echo -e -n "${HEADER}\t[+] Checking if python is installed..."
+dpkg -s python >/dev/null 2>&1
+
+if [ $? -ne 0 ]; then
+    echo -e -n "${ERROR}FAIL\n\t${HEADER}\t[-] Installing python..."
+
+    sudo apt-get install -y -qq python
+
+    if [ $? -eq 0 ]; then
+	    echo -e "${OK}OK${ENDC}"
+    else
+        echo -e "${ERROR}ERROR${ENDC}"
+    fi
+else
+    echo -e "${OK}OK${ENDC}"
+fi
+
 echo -e -n "${HEADER}\t[+] Checking if python-pip is installed..."
 dpkg -s python-pip >/dev/null 2>&1
 
 if [ $? -ne 0 ]; then
-    echo -e -n "${ERROR}FAIL\n\t${HEADER}Installing python-pip..."
+    echo -e -n "${ERROR}FAIL\n\t${HEADER}\t[-] Installing python-pip..."
 
-    sudo apt-get install -y python-pip >/dev/null 2>&1
-    
+    sudo apt-get install -y -qq python-pip
+
     if [ $? -eq 0 ]; then
-	echo -e "${OK}OK${ENDC}"
+        echo -e "${OK}OK${ENDC}"
     else
-	echo -e "${ERROR}ERROR${ENDC}"
+        echo -e "${ERROR}ERROR${ENDC}"
     fi
 else
     echo -e "${OK}OK${ENDC}"
@@ -37,12 +64,12 @@ python -c "import requests" >/dev/null 2>&1
 
 if [ $? -ne 0 ]; then
     echo -e -n "${ERROR}FAIL\n\t${HEADER}\t[-] Installing requests module..."
-    pip install requests >/dev/null 2>&1
+    pip install --user requests >/dev/null 2>&1
 
     if [ $? -eq 0 ]; then
-	echo -e "${OK}OK${ENDC}"
+        echo -e "${OK}OK${ENDC}"
     else
-	echo -e "${ERROR}ERROR${ENDC}"
+        echo -e "${ERROR}ERROR${ENDC}"
     fi
 else
     echo -e "${OK}OK${ENDC}"
@@ -51,18 +78,12 @@ fi
 echo ""
 echo -e -n "${HEADER}[++] Setting alias for control.py...${ENDC}"
 
+#TODO: check if exist the same lime
+#TODO: check if exist the same alias 'icp'. If exist make with this 'imdea-control'
+alias_def="icp"
+alias_long="imdea-control"
+alias_final="" # to set with the final alias added in .aliases
 echo "alias icp='${PWD}/control.py'" >> ~/.aliases
-
-if [ $? -eq 0 ]; then
-    echo -e "${OK}OK${ENDC}"
-else
-    echo -e "${ERROR}ERROR${ENDC}"
-fi
-
-
-echo -e -n "${HEADER}\t[+] Adding alias to .bashrc and .zshrc...${ENDC}"
-
-echo "source ~/.aliases" >> ~/.bashrc && echo "source ~/.aliases" >> ~/.zshrc
 
 if [ $? -eq 0 ]; then
     echo -e "${OK}OK${ENDC}"
@@ -75,6 +96,7 @@ echo -e -n "Installation finished, run your script with \"icp\" command"
 echo -e "${ENDC}"
 echo ""
 
-echo -e "${HEADER}[++] Running help of control.py script...${ENDC}"
+
+echo -e "${HEADER}[++] Running './control.py -h' script...${ENDC}"
 echo ""
 python ${PWD}/control.py -h
